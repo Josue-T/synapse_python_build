a35fcd70cbfc4b9a3da9622c3058aa7683f4de4c
Josué Tille <josue@tille.ch>
2017-10-27 08:22:14 +0200

    Improve Script

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ This is a part of the project :  https://github.com/YunoHost-Apps/synapse_ynh
 This is a script with provide a virtualenv already built for arm arch with all dependance. It improve the time of synapse package installation on slow arch.
 
 The script build_pyenv.sh is used to build the release package.
+
+How to
+------
+
+### Prerequist
+
+The build need a really clean environnement with no library in relation with `python-libxml2` so it's recommended to use a chroot. 
+
+### Preparation
+
+- Install the dependance :
+```
+apt install -y build-essential python2.7-dev libffi-dev python-pip python-setuptools sqlite3 libssl-dev python-virtualenv libjpeg-dev libpq-dev postgresql libgcrypt11-dev libgcrypt20-dev libxml2-dev libxslt1-dev python-lxml
+```
+
+- Clone the git repository.
+
+- Lauch the build script by this command : `bash build_pyenv.sh`
+
+- If nothing fail you should find a file named `matrix-synapse-bin-x.x.x-ARCH.tar.gz` in the same dir then your script.
+
+### Troubleshotting
+
+#### lxml build fail :
+
+In my case the solution to solve my issue was to type these both command to have a really clean environnement (IN CHOOT, because it could break a lot of things in production environnement) :
+```
+apt-get autoremove --purge python-defusedxml libxml2-utils libxml-libxml-perl libxml-namespacesupport-perl docbook-xml libxml2-dev libxml2 pinentry-curses
+apt install -y build-essential python2.7-dev libffi-dev python-pip python-setuptools sqlite3 libssl-dev python-virtualenv libjpeg-dev libpq-dev postgresql libgcrypt11-dev libgcrypt20-dev libxml2-dev libxslt1-dev python-lxml
+```
--- build_pyenv.sh
+++ build_pyenv.sh
@@ -1,49 +1,57 @@
 #!/bin/bash
 
-dir_to_build="/opt/yunohost/matrix-synpase"
+dir_to_build="/opt/yunohost/matrix-synapse"
+release_number="1"
 
 #################################################################
 
 echo "Est vous bien dans un chroot ? [y/n]"
-read $a
+read a
 if [[ $a != "y" ]]
 then
 	echo "Il est fortement conseillé d'être dans un chroot pour faire ces opérations"
 	exit 0
 fi
 
-# Install dependance
+# Upgrade system
 apt update
-apt dist-upgrade
-apt install -y build-essential python2.7-dev libffi-dev python-pip python-setuptools sqlite3 libssl-dev python-virtualenv
-
-pip install --upgrade pip
+apt dist-upgrade -y
+pip2 install --upgrade pip
 pip install --upgrade virtualenv
 
 # Clean environnement
 rm -rf $dir_to_build
 
+# Enable set to be sure that all command don't fail
+set -eu
+
 # Create new environnement
 mkdir -p $dir_to_build
-virtualenv --always-copy -p python2.7 $dir_to_build
+virtualenv --no-site-packages --always-copy -p python2.7 $dir_to_build
 cp activate_virtualenv $dir_to_build/bin/activate
 
 # Go in virtualenv
 old_pwd="$PWD"
 cd $dir_to_build
+PS1=""
 source bin/activate
 
 # Install source and build binary
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade cffi ndg-httpsclient psycopg2 lxml
-pip install --upgrade https://github.com/matrix-org/synapse/tarball/master
+pip install -I --upgrade pip
+pip install -I --upgrade setuptools
+pip install -I --upgrade cffi ndg-httpsclient psycopg2 lxml
+pip install -I --upgrade https://github.com/matrix-org/synapse/tarball/master
 
+matrix_version=$(pip list --format=columns | grep --line-buffered -E "matrix-synapse  *" | sed 's/matrix-synapse *//' |  sed 's/ *$//')
 # Quit virtualenv
 deactivate
 cd "$old_pwd"
 
 # Build archive of binary
-tar -cf "matrix-synpase-bin.tar.gz" "$dir_to_build"
+archive_name="matrix-synapse_${matrix_version}-bin${release_number}_$(uname -m).tar.gz"
+tar -cf "$archive_name" "$dir_to_build"
+
+echo "sha256 SUM :"
+sha256sum "$archive_name"
 
 exit 0
